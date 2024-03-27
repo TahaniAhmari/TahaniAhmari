2de9fc48abb80ec8725beb99f0882a337606e42a
TahaniAhmari <163916731+TahaniAhmari@users.noreply.github.com>
2024-03-28 00:47:39 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import SignInFormDeskto from './SignInFormDeskto';
+
+function App() {
+  return (
+    <div className="App">
+     <SignInFormDeskto/>
+    </div>
+  );
+}
+
+
+
+export default App; 
+
---
+++ src/SignInFormDeskto.js
@@ -0,0 +1,77 @@
+import React from 'react'
+
+export default function SignInFormDeskto() {
+  return (
+    <div>
+      <div className="sign-in-form-desktop">
+        <div className="rectangle-2756">
+        </div>
+        <div className="frame-28986">
+          <div className="frame-28984">
+            <div className="ui-unicorn-logo">
+              <div className="avatar-ui-unicorn-v-2">
+              </div>
+            </div>
+            <div className="log">
+              
+              UI Unicorn
+            </div>
+          </div>
+          <div className="sign-in-forms">
+            <div className="frame-28976">
+              <div className="">
+                Nice to see you again
+              </div>
+              <div className="frame-28968">
+                <div className="input-configurator">
+                  <div className="satellite-input">
+                    <span className="login">
+                      Login
+                    </span>
+                  </div>
+                  <div className="login-input">
+                    <div className="regular-input-double-row">
+                      <span className="email-or-phone-number">
+                        Email or phone number
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div className="input-configurator-4">
+                  <div className="satellite-input-5">
+                    <span className="password">
+                      Password
+                    </span>
+                  </div>
+                  <div className="password-input">
+                    <div className="regular-input-double-row-7">
+                      <span className="enter-password">
+                        Enter password
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="description-2">
+              Forgot password?
+            </div>
+            <div className="primary-button-40-px">
+              <span className="dis">
+                Sign in
+              </span>
+            </div>
+            <div className="sign-up-offer">
+              <span className="description">
+                Dont have an account?
+              </span>
+              <span className="description-1">
+                Sign up now
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
